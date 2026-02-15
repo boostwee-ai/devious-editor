8e8d66b860910b62bcbdedc501f3b5432f67ba7e
boostwee-ai <boostwee@gmail.com>
2026-02-15 02:55:32 -0500

    Refactor includes and clean up server browsing code
    Reorganized includes and fixed comments for clarity. Removed unused code related to server browsing and multiplayer functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
-// --- WINDOWS HEADERS MUST BE FIRST ---
+#include <Geode/Geode.hpp>
+
+// --- WINDOWS NETWORKING SHIELD (MUST BE AT THE TOP) ---
 #ifdef GEODE_IS_WINDOWS
     #ifndef WIN32_LEAN_AND_MEAN
         #define WIN32_LEAN_AND_MEAN
@@ -7,7 +9,6 @@
     #include <ws2tcpip.h>
 #endif
 
-#include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
 #include <Geode/modify/LevelEditorLayer.hpp>
 #include <Geode/modify/EditorPauseLayer.hpp>
@@ -20,8 +21,8 @@ class ServerBrowser : public FLAlertLayer {
     CCMenu* m_listMenu;
 public:
     bool init() {
-        // 9th argument (1.0f) for Geode v5
-        if (!FLAlertLayer::init(nullptr, "LAN Servers", "Searching...", "Cancel", nullptr, 300.0f, false, 0, 1.0f)) 
+        // FIX: Added '1.0f' as the 9th argument for Geode v5
+        if (!FLAlertLayer::init(nullptr, "LAN Servers", "Searching for hosts...", "Cancel", nullptr, 300.0f, false, 0, 1.0f)) 
             return false;
         
         m_listMenu = CCMenu::create();
@@ -100,7 +101,7 @@ class $modify(MyPauseLayer, EditorPauseLayer) {
 };
 
 class $modify(MyEditor, LevelEditorLayer) {
-    // FIX: Using createObject instead of addObject prevents the "no member" error
+    // FIX: Using createObject to avoid 'no member' error on Mac
     GameObject* createObject(int id, CCPoint pos, bool undo) {
         GameObject* obj = LevelEditorLayer::createObject(id, pos, undo);
         if (obj) {
@@ -109,71 +110,5 @@ class $modify(MyEditor, LevelEditorLayer) {
             NetworkManager::get()->sendPacket(ss.str());
         }
         return obj;
-    }
-};
-            btn->setUserObject(CCString::create(s.ip));
-            m_listMenu->addChild(btn);
-        }
-        m_listMenu->updateLayout();
-    }
-
-    void onJoin(CCObject* sender) {
-        auto node = static_cast<CCNode*>(sender);
-        if (auto ipStr = dynamic_cast<CCString*>(node->getUserObject())) {
-            NetworkManager::get()->connectToServer(ipStr->getCString());
-            this->onBtn1(nullptr); 
-        }
-    }
-
-    static ServerBrowser* create() {
-        auto ret = new ServerBrowser();
-        if (ret && ret->init()) { ret->autorelease(); return ret; }
-        CC_SAFE_DELETE(ret);
-        return nullptr;
-    }
-};
-
-// --- HOOK 1: MAIN MENU WIFI BUTTON ---
-class $modify(MyMenuLayer, MenuLayer) {
-    bool init() {
-        if (!MenuLayer::init()) return false;
-        auto menu = this->getChildByID("bottom-menu");
-        auto sprite = CCSprite::createWithSpriteFrameName("GJ_everyplayBtn_001.png");
-        if (!sprite) sprite = ButtonSprite::create("LAN", 40, true, "goldFont.fnt", "GJ_button_01.png", 30, 0.6f);
-        auto btn = CCMenuItemSpriteExtra::create(sprite, this, menu_selector(MyMenuLayer::onMultiplayer));
-        menu->addChild(btn);
-        menu->updateLayout();
-        return true;
-    }
-    void onMultiplayer(CCObject*) { ServerBrowser::create()->show(); }
-};
-
-// --- HOOK 2: EDITOR PAUSE (HOST BUTTON) ---
-class $modify(MyPauseLayer, EditorPauseLayer) {
-    void customSetup() {
-        EditorPauseLayer::customSetup();
-        auto menu = this->getChildByID("center-menu");
-        auto btnSprite = ButtonSprite::create("Host LAN", 80, true, "goldFont.fnt", "GJ_button_01.png", 30, 0.6f);
-        auto btn = CCMenuItemSpriteExtra::create(btnSprite, this, menu_selector(MyPauseLayer::onHost));
-        menu->addChild(btn);
-        menu->updateLayout();
-    }
-    void onHost(CCObject*) {
-        std::string levelName = "Unknown Level";
-        if (auto level = LevelEditorLayer::get()->m_level) levelName = level->m_levelName;
-        NetworkManager::get()->startHost(levelName);
-    }
-};
-
-// --- HOOK 3: LIVE OBJECT SYNC ---
-class $modify(MyEditor, LevelEditorLayer) {
-    void addObject(GameObject* obj) {
-        // FIX: Explicitly call base class method to fix scope errors
-        this->LevelEditorLayer::addObject(obj);
-        if (obj->getTag() == 99999) return;
-        
-        std::stringstream ss;
-        ss << "1," << obj->m_objectID << "," << obj->getPositionX() << "," << obj->getPositionY();
-        NetworkManager::get()->sendPacket(ss.str());
     }
 };
